e2f23b6a158a5b5cf43b7385db5669d08a8b06b3
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2021-05-25 13:54:55 +0900

    gh-pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=krw&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => {
       setCoins(res.data);
-      console.log(res.data);
     }).catch(error => console.log(error))
   }, []);
 
